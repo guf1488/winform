b456f83d6715c4809d57caf58a5fb7aaa53eb609
Artem Dadychenko <artem_dadychenko@mail.ru>
2017-09-15 14:43:01 +0300

    q

--- WinForm/WinForm/Form1.cs
+++ WinForm/WinForm/Form1.cs
@@ -17,5 +17,45 @@ namespace WinForm
             // ekz
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string result = "";
+            if (textBox1.Text.Length < 6 || textBox1.Text.Length > 12)
+            {
+                result = "Неверная длина пароля";
+                MessageBox.Show(result);
+                return;
+            }
+            bool checkDigit = false;
+            bool checkNum = false;
+            foreach (var item in textBox1.Text)
+            {
+                checkDigit = checkDigit || char.IsDigit(item);
+            }
+            foreach (var item in textBox1.Text)
+            {
+                checkNum = checkNum || char.IsLetter(item);
+            }
+            if (!(checkNum && checkDigit))
+            {
+                result += "В пароле только буквы или только цифры";
+                MessageBox.Show(result);
+            }
+
+
+            if (string.Equals(textBox1.Text, textBox2.Text))
+            {
+                result += "Пароли одинаковые";
+                MessageBox.Show(result);
+            }
+            else
+            {
+                result += "Пароли не одинаковые";
+                MessageBox.Show(result);
+            }
+
+            
+        }
     }
 }
